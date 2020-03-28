34f096dc66affe45ea293ef514947e2a27c156b0
SaigyoujiYuyuko <HGK-SaigyoujiYuyuko@outlook.com>
2020-03-28 21:50:16 +0800

    支持配置文件

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 document.addEventListener('DOMContentLoaded',function(){
 
+    // init
+    document.title = env.title;
+    $('.title').html(env.title);
+    $('.subtitle').html(env.subtitle);
+    $('.body').html(env.body);
+
     // vars
     let animationendFlag = false;
 
     // load images
     const img = new Image();
-    img.src = "bg.png";
+    img.src = env.bgImages[randomNum(0, env.bgImages.length - 1)];
 
     // background
     const bgCover = $('.background-cover');
@@ -20,7 +26,21 @@ document.addEventListener('DOMContentLoaded',function(){
 
     img.addEventListener('load', function () {
         console.log("load");
-        bg.css('background-image', img.baseURI);
+        bg.css('background-image', "url('" + img.src + "')");
         bgCover.addClass('animated fadeOut slower');
     }, false);
 });
+
+function randomNum(minNum,maxNum){
+    switch(arguments.length){
+        case 1:
+            return parseInt(Math.random()*minNum+1,10);
+            break;
+        case 2:
+            return parseInt(Math.random()*(maxNum-minNum+1)+minNum,10);
+            break;
+        default:
+            return 0;
+            break;
+    }
+}
