dfaeb7bf9eeb1f865f8af10bfe25ed82edb52026
SaigyoujiYuyuko <HGK-SaigyoujiYuyuko@outlook.com>
2020-03-28 20:17:14 +0800

    添加背景遮罩/渐变

---
+++ app.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded',function(){
+
+    // vars
+    let animationendFlag = false;
+
+    // load images
+    const img = new Image();
+    img.src = "bg.png";
+
+    // background
+    const bgCover = $('.background-cover');
+    const bg = $(".background");
+
+    // display images
+    const element =  document.querySelector('.header');
+
+    element.addEventListener('animationend', function() {
+        animationendFlag = true;
+    });
+
+    img.addEventListener('load', function () {
+        console.log("load");
+        bg.css('background-image', img.baseURI);
+        bgCover.classList.add('animated', 'fadeOut');
+    }, false);
+});
