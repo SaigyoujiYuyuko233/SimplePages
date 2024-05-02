31247ba91580d8b29fb2e030a386851c6a1a2483
西行寺幽幽子 <39913604+SaigyoujiYuyuko233@users.noreply.github.com>
2024-05-02 14:35:47 +0800

    for pages

---
+++ env.js
@@ -0,0 +1,19 @@
+export const env = {
+
+    // website title
+    websiteTitle: 'Simple-Pages',
+
+    // title in middle
+    title: 'Simple Pages',
+
+    // subtitle under the title
+    subtitle: 'By SaigyoujiYuyuko233',
+
+    // body under the line separator
+    body: '<p><a href="https://github.com/SaigyoujiYuyuko233/SimplePages">Github</a></p>',
+
+    // background images (random display)
+    bgImages: [
+        'images/bg.png',
+    ]
+};
